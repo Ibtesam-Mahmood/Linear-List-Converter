2aa0ce15b9ec97567a2f6d0ae3aa30b9b3bf28e6
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2017-12-23 00:21:55 -0500

    Added functionality to check if base directory is empty

--- src/FileReader.java
+++ src/FileReader.java
@@ -1,4 +1,36 @@
+import java.io.File;
 
 public class FileReader {
 
+	private final String readDir = "C:\\Users\\ibtes\\Documents\\WorkSpaces\\Mini Functions\\Linear List Converter\\text files\\Base";
+	private final String postDir = "C:\\Users\\ibtes\\Documents\\WorkSpaces\\Mini Functions\\Linear List Converter\\text files\\Converted";
+	
+	private String spacing;
+	
+	public FileReader(String spacing) {
+		this.spacing = spacing;
+	}
+	
+	public void read() {
+		
+		if(!checkEmptyDirectory())
+			return;
+		
+		
+		
+	}
+	
+	private boolean checkEmptyDirectory() {
+		
+		File[] files = (new File(readDir)).listFiles();
+		
+		if(files.length == 0) {
+			System.out.println("No Files to Convert");
+			return false;
+		}
+		else
+			return true;
+		
+	}
+	
 }
